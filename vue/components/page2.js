d3d33d8f7130e0e9401c9129e57a898da43f892e
jpolvora <jpolvora@gmail.com>
2017-11-10 16:10:02 -0300

    axios

--- vue/components/page2.js
+++ vue/components/page2.js
@@ -1,11 +1,26 @@
 define((require) => {
     const Vue = require('vue');
+    var axios = require('axios');
 
     var component = Vue.component('page2', {
-        template: "<h1>Page2</h1>",
-       
+        template: "<div><h1>Page2</h1><p>{{ message }}</p></div>",
+
+        data: function () {
+            return {
+                message: "..."
+            };
+        },
         created: function () {
             console.info('page2 component created');
+        },
+
+        mounted: function () {
+            var self = this;
+            axios.get('/services').then((response) => {
+                self.message = response.statusText;
+            }).catch((error) => {
+                self.message = error.response.statusText;
+            });
         }
     });
 
--- vue/require.config.js
+++ vue/require.config.js
@@ -4,6 +4,7 @@ require.config({
         vue: '../node_modules/vue/dist/vue',
         'vue-router': '../node_modules/vue-router/dist/vue-router',
         text: "../node_modules/text/text",
+        axios: "../node_modules/axios/dist/axios",
     },
     deps: ["shell"]
 });
