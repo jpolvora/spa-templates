9a1fe320c6d7728d609f4691c543ee88b26b6cad
jpolvora <jpolvora@gmail.com>
2017-11-13 15:24:11 -0300

    gulp tasks init.

---
+++ vue/gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+
+gulp.task('build', function () {
+    console.log('the build task.');
+});
+
+gulp.task('default', function () {
+    // place code for your default task here
+    console.log('choose a task.');
+});
