d0c4117b78ecc34e825f6ae1ba83ab5224f64a5b
jpolvora <jpolvora@gmail.com>
2017-11-12 21:44:52 -0300

    ok

--- vue/require.config.js
+++ vue/require.config.js
@@ -4,7 +4,7 @@ require.config({
         vue: '../node_modules/vue/dist/vue.min',
         'vue-router': '../node_modules/vue-router/dist/vue-router.min',
         text: "../node_modules/text/text",
-        axios: "../node_modules/axios/dist/axios",
+        axios: "../node_modules/axios/dist/axios"
     },
     deps: ["shell"]
 });
